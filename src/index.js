e15b011640ddd4b7392535c4203ac572cbaefe33
erichvorster <erichvorster1@gmail.com>
2022-04-11 18:33:00 +0200

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return (
+    <div className ="ui container comments">
+        <CommentDetail author="Tim" time="Today at 4:45PM" comment="This is cool" image={faker.image.image()}/>
+    </div>
+    )};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App/>, document.querySelector('#root'));
+
